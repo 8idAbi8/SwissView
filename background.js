1f87de79e79bddfdd375b800dcd262df2783a6e6
idabi8 <i.l.t.l.i@hotmail.it>
2024-12-14 22:58:12 +0100

    onclick incon - open new tab

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
-// This file can be used in the future for background tasks
+/* 
+chrome.action.onClicked.addListener: This event is triggered when the extension icon is clicked.
+
+chrome.tabs.create: Opens a new tab. The url parameter specifies the location to load in the new tab. Here, we load the default new tab (which your extension overrides).
+*/
+
+chrome.action.onClicked.addListener(() => {
+    // Open a new tab with the custom new tab page
+    chrome.tabs.create({ url: "chrome://newtab" });
+});
+  
